20048b077cfc930af14b3952934fceb20d827e98
Gallien L <1328920+glb2154@users.noreply.github.com>
2020-03-23 23:11:06 +0100

    example

--- example4.php
+++ example4.php
@@ -2,6 +2,9 @@
 require 'vendor/autoload.php';
 use Zend\EventManager\SharedEventManager;
 
+/**
+ * This code is saying, "Listen to the 'do' event of the 'Example' target, and, when notified, execute this callback."
+ */
 $sharedEvents = new SharedEventManager();
 $sharedEvents->attach('Example', 'do', function ($e) {
     $event  = $e->getName();
